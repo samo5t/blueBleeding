069faeaf78413410125bfde6b419663389bf1dd4
samo5t <abusecheat@mail.ru>
2024-08-08 00:23:09 +0300

    123

--- registration.py
+++ registration.py
@@ -3,14 +3,14 @@ from flask import Flask, request, render_template, Blueprint
 app = Flask(__name__)
 
 
-@app.route("/register")
+@app.route("/register", methods=["GET"])
 def register():
     # username = request.form.get("username")
     # password = request.form.get("password")
     #
     # # Добавьте свой код для обработки регистрации
 
-    return render_template('/site/index.html', msg='')
+    return render_template('RegistrationWindow.html', msg='')
 
 
 if __name__ == "__main__":
